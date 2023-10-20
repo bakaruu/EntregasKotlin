1c70c2e1c201086ebbf646bd6345d3e122e88ec6
Alvaro Castro <arucastro@gmail.com>
2023-10-20 16:28:05 +0200

    Subida proyecto seminario1

---
+++ seminario1/src/main/kotlin/Main.kt
@@ -0,0 +1,46 @@
+fun main(args: Array<String>) {
+
+
+    //Ejercicio1
+    
+
+    //Ejercicio1
+    //Ejercicio2
+    //Ejercicio3
+    //Ejercicio4
+    //Ejercicio5
+    //Ejercicio6
+    //Ejercicio7
+    //Ejercicio8
+    //Ejercicio9
+    //Ejercicio10
+    //Ejercicio11
+    //Ejercicio12
+    //Ejercicio13
+    //Ejercicio14
+    //Ejercicio15
+    //Ejercicio16
+    //Ejercicio17
+    //Ejercicio18
+    //Ejercicio19
+    //Ejercicio20
+    //Ejercicio21
+    //Ejercicio22
+    //Ejercicio24
+    //Ejercicio25
+    //Ejercicio26
+    //Ejercicio27
+    //Ejercicio28
+    //Ejercicio29
+    //Ejercicio30
+    //Ejercicio31
+    //Ejercicio32
+    //Ejercicio33
+    //Ejercicio34
+    //Ejercicio35
+
+
+
+
+
+}
